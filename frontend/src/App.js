a8e21e5c818184f68d1e48358905ffaec3946a08
shahariartushar <shahariartushar@gmail.com>
2022-11-03 00:03:57 +0100

    added HomeScreen with some sample data

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,20 @@
+import React from "react"
+import { Container } from "react-bootstrap"
+
+import HomeScreen from "./screens/HomeScreen"
+import Header from "./components/Header"
+import Footer from "./components/Footer"
+
 const App = () => {
   return (
     <>
-      <h1>Hello</h1>
+      <Header />
+      <main className='py-3'>
+        <Container>
+          <HomeScreen></HomeScreen>
+        </Container>
+      </main>
+      <Footer />
     </>
   )
 }
