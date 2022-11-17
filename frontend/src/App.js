cccad59d2aa24fca92a238412c8fc27735b67440
shahariartushar <shahariartushar@gmail.com>
2022-11-17 23:36:03 +0100

    added React Rounter Dom  and ProductScreen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,32 @@
 import React from "react"
 import { Container } from "react-bootstrap"
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
 
 import HomeScreen from "./screens/HomeScreen"
 import Header from "./components/Header"
 import Footer from "./components/Footer"
+import ProductScreen from "./screens/ProductScreen"
 
 const App = () => {
   return (
-    <>
+    <Router>
       <Header />
       <main className='py-3'>
         <Container>
-          <HomeScreen></HomeScreen>
+          <Routes>
+            <Route
+              path='/'
+              element={<HomeScreen />}
+            />
+            <Route
+              path='/products/:id'
+              element={<ProductScreen />}
+            />
+          </Routes>
         </Container>
       </main>
       <Footer />
-    </>
+    </Router>
   )
 }
 
